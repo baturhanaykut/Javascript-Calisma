09b8525147a274e273404d766a803e2e92469323
Baturhan Aykut <baturhanaykut@gmail.com>
2024-03-06 23:31:07 +0300

    Bölüm-18 Side Bar Projesi

---
+++ Bölüm-18 SideBar Projesi/script.js
@@ -0,0 +1,11 @@
+const bars=document.querySelector('.fa-bars');
+const sidebar=document.querySelector('.sidebar');
+const xmark = document.querySelector('.fa-xmark');
+
+bars.addEventListener('click',()=>{
+    sidebar.classList.toggle('showSidebar');
+});
+
+xmark.addEventListener('click',()=>{
+    sidebar.classList.remove('showSidebar');
+});
